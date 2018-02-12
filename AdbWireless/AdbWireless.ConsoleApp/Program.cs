e63656fc93a8b52af9cb6614fb68e4f9bdb1ea1e
Kyle John <njk230@gmail.com>
2018-02-13 01:42:29 +0900

    Add sample code

---
+++ AdbWireless/AdbWireless.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdbWireless.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isSuccess = AdbConnecter.Connect("192.168.219.104").Result;
+        }
+    }
+}
