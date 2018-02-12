307285223715f841ab26a983da8e0eb8ea8dbe9f
Kyle John <njk230@gmail.com>
2018-02-13 01:41:19 +0900

    Add AdbConnecter class

---
+++ AdbWireless/AdbWireless/AdbConnecter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdbWireless
+{
+    public class AdbConnecter
+    {
+        static Regex regex = new Regex($"connected to (.+?){Environment.NewLine}");
+
+        public static async Task<bool> Connect(string ipAddress)
+        {
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = "adb",
+                Arguments = $"connect {ipAddress}",
+                RedirectStandardOutput = true
+            });
+
+            var output = await process.StandardOutput.ReadToEndAsync();
+            bool isSuccess = ParseAdbResponse(output);
+
+            return isSuccess;
+        }
+
+        static bool ParseAdbResponse(string response)
+        {
+            return regex.IsMatch(response);
+        }
+    }
+}
--- AdbWireless/AdbWireless/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AdbWireless
-{
-    public class Class1
-    {
-    }
-}
