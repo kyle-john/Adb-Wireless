10f2fec192a3a9b064c72bf078128a317cfd6d74
Kyle John <njk230@gmail.com>
2018-02-13 01:42:18 +0900

    Add AndroidDeivceBrowser

---
+++ AdbWireless/AdbWireless/AndroidDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdbWireless
+{
+    public class AndroidDevice
+    {
+        public string Name { get; set; }
+
+        public string IPAddress { get; set; }
+    }
+}
